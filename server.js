84da3cf847c05644c21d3a4df430b85be3fd66d5
Brian Erickson <djbriane@gmail.com>
2013-01-10 11:18:15 -0700

    removed absolute path from node-static and fixed minor JS syntax issues

--- server.js
+++ server.js
@@ -11,71 +11,73 @@ var sys = require('sys');
 var http = require('http');
 
 // Use node-static module to server chart for client-side dynamic graph
-var nodestatic = require('/usr/local/lib/node_modules/node-static');
+var nodestatic = require('node-static');
 
 // Setup static server for current directory
 var staticServer = new nodestatic.Server(".");
 
 // Setup node http server
 var server = http.createServer(
-	// Our main server function
-	function(request, response)
-	{
-		// Grab the URL requested by the client
-		var url = require('url').parse(request.url);
-		var pathfile = url.pathname;
+  // Our main server function
+  function(request, response)
+  {
+    // Grab the URL requested by the client
+    var url = require('url').parse(request.url);
+    var pathfile = url.pathname;
 
-		// Test to see if it's a request for temperature data
-		if (pathfile == '/temperature.json')
-		{
-			// Function to read thermal sensor and return JSON representation of first word (i.e. the data)
-			// Note device location is sensor specific.
-			fs.readFile('/sys/bus/w1/devices/28-00000400a88a/w1_slave', function(err, buffer)
-			{
-				if (err)
-				{
-					response.writeHead(500, { "Content-type": "text/html" });
-					response.end(err + "\n");
-					return;
-				}
-			// Read data from file (using fast node ASCII encoding).
-			var data = buffer.toString('ascii').split(" "); // Split by space
+    // Test to see if it's a request for temperature data
+    if (pathfile === '/temperature.json')
+    {
+      // Function to read thermal sensor and return JSON representation of first word (i.e. the data)
+      // Note device location is sensor specific.
+      fs.readFile('/sys/bus/w1/devices/28-00000400a88a/w1_slave', function(err, buffer)
+      {
+        if (err)
+        {
+          response.writeHead(500, { "Content-type": "text/html" });
+          response.end(err + "\n");
+          return;
+        }
+      // Read data from file (using fast node ASCII encoding).
+      var data = buffer.toString('ascii').split(" "); // Split by space
 
-            		// Extract temperature from string and divide by 1000 to give celsius
-			var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
-			
-			// Round to one decimal place
-			temp = Math.round(temp * 10) / 10
-			
-			// Add date/time to temperature
-			var jsonData = [Date.now(), temp];
-			
-			// Return JSON data	
-			response.writeHead(200, { "Content-type": "application/json" });		
-			response.end(JSON.stringify(jsonData), "ascii");
-			// Log to console (debugging)
-			// console.log('returned JSON data: ' + jsonData);
-				
-			});
-		}
-		else {
-      	// Print requested file to terminal
-		   console.log('Request from '+ request.connection.remoteAddress +', for: ' + pathfile);
+      // Extract temperature from string and divide by 1000 to give celsius
+      var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
 
-         // Serve file using node-static			
+      // Round to one decimal place
+      temp = Math.round(temp * 10) / 10;
+
+      // Add date/time to temperature
+      var jsonData = [Date.now(), temp];
+
+      // Return JSON data
+      response.writeHead(200, { "Content-type": "application/json" });
+      response.end(JSON.stringify(jsonData), "ascii");
+      // Log to console (debugging)
+      // console.log('returned JSON data: ' + jsonData);
+
+      });
+    }
+    else {
+        // Print requested file to terminal
+       console.log('Request from '+ request.connection.remoteAddress +', for: ' + pathfile);
+
+         // Serve file using node-static
          staticServer.serve(request, response, function (err, result) {
-					if (err){
-						// Log the error
-						sys.error("Error serving " + request.url + " - " + err.message);
-						
-						// Respond to the client
-						response.writeHead(err.status, err.headers);
-						response.end('Error 404 - file not found');
-						}
-					})
-		}
+          if (err){
+            // Log the error
+            sys.error("Error serving " + request.url + " - " + err.message);
+
+            // Respond to the client
+            response.writeHead(err.status, err.headers);
+            response.end('Error 404 - file not found');
+            }
+          });
+    }
 });
+
 // Enable server
 server.listen(8000);
+
 // Log message
 console.log('Server running at http://localhost:8000');
