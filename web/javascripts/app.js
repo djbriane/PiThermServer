c788caf48591353a17659c4e0292fa9016e24a12
Brian Erickson <djbriane@gmail.com>
2013-04-09 10:26:50 -0600

    Added current temp display

--- web/javascripts/app.js
+++ web/javascripts/app.js
@@ -4,11 +4,24 @@
   var $doc = $(document),
       Modernizr = window.Modernizr;
 
-  var graph, sensorData = [];
+  var graph, sensorData = [],
+      sparkFormat = {
+          width: '100%',
+          height: '80px',
+          lineColor: '#0b62a4',
+          highlightLineColor: false,
+          fillColor: false,
+          spotRadius: 3,
+          normalRangeMax: 72.0,
+          normalRangeMin: 60.0,
+          lineWidth: 2,
+          tooltipSuffix: '°',
+          numberFormatter: function(val) {return Math.round(val); }
+        };
 
   // Get data from Pi NodeJS server
   function getSensorData() {
-    var sensorData, sensorData24, sensorData48, time12hrsago, time24hrsago, time48hrsago;
+    var sensorData12, sensorData24, sensorData48, time12hrsago, time24hrsago, time48hrsago;
 
     time12hrsago = new window.moment().subtract('hours', 12);
     time24hrsago = new window.moment().subtract('hours', 24);
@@ -16,17 +29,17 @@
 
     console.log('get sensor data');
     $.getJSON('./temperature.json', function(data) {
-      sensorData = data;
-
       if (data && data.length > 0) {
-        data = _.filter(data, function(val, index) {
+        data = _.filter(data, function(val) { return (val.value > 50.0 && val.value < 90.0); });
+
+        sensorData12 = _.filter(data, function(val, index) {
           return (time12hrsago.isBefore(val.time) && index % 2 === 0);
         });
 
         if (!graph) {
           graph = window.Morris.Line({
             element: 'line-example',
-            data: _.filter(data, function(val) { return (val.value > 50.0 && val.value < 90.0); }),
+            data: sensorData12,
             xkey: 'time',
             ykeys: ['value'],
             labels: ['Temp Sensor'],
@@ -41,49 +54,25 @@
             dateFormat: function (x) { return new window.moment(x).format('MMM Do, h:mm:ss a'); }
           });
         } else {
-          graph.setData(data);
+          graph.setData(sensorData12);
         }
 
-        sensorData24 = _.filter(sensorData, function(val, index) {
+        // setup data for 24hr graph
+        sensorData24 = _.filter(data, function(val, index) {
           return (time24hrsago.isBefore(val.time) && index % 12 === 0);
         });
         sensorData24 = _.pluck(sensorData24, 'value').reverse();
-        sensorData24 = _.filter(sensorData24, function(num) { return (num > 50.0 && num < 90.0); });
+        $('.graph-24hr').sparkline(sensorData24, sparkFormat);
 
+        // setup data for 48hr graph
         sensorData48 = _.filter(sensorData, function(val, index) {
           return (time48hrsago.isBefore(val.time) && index % 24 === 0);
         });
         sensorData48 = _.pluck(sensorData48, 'value').reverse();
-        sensorData48 = _.filter(sensorData48, function(num) { return (num > 50.0 && num < 90.0); });
+        $('.graph-48hr').sparkline(sensorData48, sparkFormat);
 
+        $('.current-temp').text(Math.round(_first(data).value) + '°');
 
-        $('.graph-24hr').sparkline(sensorData24, {
-          width: '100%',
-          height: '80px',
-          lineColor: '#0b62a4',
-          highlightLineColor: false,
-          fillColor: false,
-          spotRadius: 3,
-          normalRangeMax: 72.0,
-          normalRangeMin: 60.0,
-          lineWidth: 2,
-          tooltipSuffix: '°',
-          numberFormatter: function(val) {return Math.round(val);}
-        });
-
-        $('.graph-48hr').sparkline(sensorData48, {
-          width: '100%',
-          height: '80px',
-          lineColor: '#0b62a4',
-          highlightLineColor: false,
-          fillColor: false,
-          spotRadius: 3,
-          normalRangeMax: 72.0,
-          normalRangeMin: 60.0,
-          lineWidth: 2,
-          tooltipSuffix: '°',
-          numberFormatter: function(val) {return Math.round(val);}
-        });
       }
 
     });
