8f2ccff993c80949fb3e213e7894ce7eff6fea1d
Brian Erickson <djbriane@gmail.com>
2013-04-18 13:20:05 -0600

    added new endpoint for showing 14d view of temp data

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ var TEMP_SENSOR_ID = '28-0000047505a4'; // DS18B20 (black plastic cap)
 
 
 // maximum number of datapoints to return
-var MAX_HISTORY = 1440;
+var SHORT_HISTORY = 2880, // 48h
+    LONG_HISTORY = 20160; // 14d
+
 
 // Use node-static module to serve chart for client-side dynamic graph
 var nodestatic = require('node-static'),
@@ -42,12 +44,47 @@ var nodestatic = require('node-static'),
 
 // Setup static server for current directory
 var staticServer = new nodestatic.Server("/home/pi/PiThermServer/web/");
-var lastRequestTime = moment().subtract('hours', 1);
+
+// get sensor data from Redis db
+function getSensorData(history, cb) {
+
+  dbclient.zrevrangebyscore([TEMP_SENSOR_ID, '+inf', '-inf', 'LIMIT', 0, history], function(err, res) {
+
+    var temp, resParsed, resData = [];
+
+    //console.log('results: ', res);
+
+    for (var i = 0, l = res.length; i < l; i = i + 1) {
+      resParsed = JSON.parse(res[i]);
+
+      temp = parseFloat(resParsed.value) / 1000.00;
+
+      // Round to one decimal place
+      temp = Math.round(temp * 10) / 10;
+
+      // Convert temp to Fahrenheit
+      temp = (temp * 1.8000) + 32.00;
+
+      resParsed.value = temp;
+
+      resData.push(resParsed);
+
+    }
+
+    if (!err) {
+      cb(null, resData);
+    } else {
+      cb(err, []);
+    }
+
+  });
+
+}
 
 // Setup node http server
 var server = http.createServer(
-// Our main server function
 
+// Our main server function
 function(request, response) {
   // Grab the URL requested by the client
   var url = require('url').parse(request.url),
@@ -62,51 +99,39 @@ function(request, response) {
       '] Request from ' + (request.connection.remoteAddress + '').magenta +
       ' for: ' + (pathfile + '').yellow);
 
-    dbclient.zrevrangebyscore([TEMP_SENSOR_ID, '+inf', '-inf', 'LIMIT', 0, MAX_HISTORY], function(err, res) {
-
-      var temp, resParsed, resData = [];
-
-      //console.log('results: ', res);
-
-      for (var i = 0, l = res.length; i < l; i = i + 1) {
-        resParsed = JSON.parse(res[i]);
-
-        temp = parseFloat(resParsed.value) / 1000.00;
-
-        // Round to one decimal place
-        temp = Math.round(temp * 10) / 10;
-
-        // Convert temp to Fahrenheit
-        temp = (temp * 1.8000) + 32.00;
-
-        resParsed.value = temp;
-
-        resData.push(resParsed);
-
-      }
+    getSensorData(SHORT_HISTORY, function(err, resData) {
 
       response.writeHead(200, {
         "Content-type": "application/json"
       });
-      if (!res) {
-        res = [];
-      }
-      response.end(JSON.stringify(resData), "ascii");
 
-      lastRequestTime = moment();
+      response.end(JSON.stringify(resData), "ascii");
 
     });
 
+  } else if (pathfile === '/temperature_long.json') {
 
-    // Extract temperature from string and divide by 1000 to give celsius
-    //var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+    // Print requested file to terminal
+    util.puts('[' + moment().format('MMMM Do YYYY, h:mm:ss a').blue +
+      '] Request from ' + (request.connection.remoteAddress + '').magenta +
+      ' for: ' + (pathfile + '').yellow);
 
-    // Round to one decimal place
-    //temp = Math.round(temp * 10) / 10;
+    getSensorData(LONG_HISTORY, function(err, resData) {
+      var time14dago = new moment().subtract('days', 14);
 
-    // Convert temp to Fahrenheit
-    //temp = (temp * 1.8000) + 32.00;
+      // only return data in 30m intervals
+      // TODO: should average across the interval instead of just plucking the value
+      resData = _.filter(resData, function(val, index) {
+        return (time14dago.isBefore(val.time) && index % 30 === 0);
+      });
 
+      response.writeHead(200, {
+        "Content-type": "application/json"
+      });
+
+      response.end(JSON.stringify(resData), "ascii");
+
+    });
 
   } else {
     // Print requested file to terminal
