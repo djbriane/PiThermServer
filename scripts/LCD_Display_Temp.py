a508b374b68e26ab35eef46e3ea377fa39bce768
Brian Erickson <djbriane@gmail.com>
2013-11-09 12:56:22 -0700

    Added dual sensor support

--- scripts/LCD_Display_Temp.py
+++ scripts/LCD_Display_Temp.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python
 
 from Adafruit_CharLCD import Adafruit_CharLCD
-#from subprocess import *
-from time import sleep
+from subprocess import *
+from time import sleep, strftime
 from datetime import datetime
 
 lcd = Adafruit_CharLCD()
 
-#cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
 
 #sensorId = "28-00000418941a"  # bare
-sensorId = "28-0000047505a4"  # black plastic
-#sensorId = "28-000002aa9557"  # silver thermowell
+sensorA = "28-0000047505a4"  # black plastic
+sensorB = "28-000002aa9557"  # silver thermowell
 
 lcd.begin(16, 1)
 
-# def run_cmd(cmd):
-#         p = Popen(cmd, shell=True, stdout=PIPE)
-#         output = p.communicate()[0]
-#         return output
+def run_cmd(cmd):
+	p = Popen(cmd, shell=True, stdout=PIPE)
+	output = p.communicate()[0]
+	return output
 
-while 1:
-        tfile = open("/sys/bus/w1/devices/%s/w1_slave" % (sensorId))
+def get_sensor_data(sensorId):
+	tfile = open("/sys/bus/w1/devices/%s/w1_slave" % (sensorId))
         text = tfile.read()
         sensorData = text.split("\n")[1].split(" ")[9]
         sensorReading = float(sensorData[2:])
         sensorReading = sensorReading / 1000
 
         sensorReadingF = 9.0 / 5.0 * sensorReading + 32
+	return sensorReadingF
 
-        print "Sensor Reading:", sensorReadingF
-
+while 1:
         lcd.clear()
         #ipaddr = run_cmd(cmd)
+	#lcd.message('IP %s' % ( ipaddr ) )
         lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
-        lcd.message("Temp: %0.1f F" % sensorReadingF)
-        #lcd.message('IP %s' % ( ipaddr ) )
+        
+	sensorReadingA = get_sensor_data(sensorA)
+	sensorReadingB = get_sensor_data(sensorB)
+	lcd.message("%0.1f / %0.1f" % (sensorReadingA, sensorReadingB))
+        
         sleep(60)
