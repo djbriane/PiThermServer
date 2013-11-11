ff36b5244dcef266f44e76c0de6be8760913747a
Brian Erickson <djbriane@gmail.com>
2013-11-11 16:03:20 -0700

    Updated web server to support multiple sensors (for real this time)

--- web/javascripts/app.js
+++ web/javascripts/app.js
@@ -21,6 +21,59 @@
 
   var MAX_RANGE = 90.0, MIN_RANGE = 50.0;
 
+  function getTrendWithSensorData(data) {
+    var trendItems = 20, currentReading, recentAvg;
+
+    currentReading = _.first(data).value;
+    recentAvg = _.reduce(_.first(data, trendItems), function(sum, item) {
+      return sum + item.value;
+    }, 0);
+
+    recentAvg = recentAvg / trendItems;
+    console.log('Trend Data - Avg: ' + recentAvg + ' Latest: ' + currentReading);
+
+    if (recentAvg < currentReading) {
+      // Trend is RISING
+      return 1;
+    }
+    if (recentAvg > currentReading) {
+      // Trend is FALLING
+      return -1;
+    }
+    // No Change / Error
+    return 0;
+  }
+
+  function updateCurrentWithResponse(response) {
+    var colorUp = '#2DB200';
+    var colorDown = '#B20000';
+    // remove any outlier data (bad sensor readings)
+    var dataSensor1 = _.filter(response[0], function(val) { return (val.value > MIN_RANGE && val.value < MAX_RANGE); });
+    var dataSensor2 = _.filter(response[1], function(val) { return (val.value > MIN_RANGE && val.value < MAX_RANGE); });
+
+    var currentSensor1 = _.first(dataSensor1);
+    var currentSensor2 = _.first(dataSensor2);
+    $('.current-temp.sensor1').text(Math.round(currentSensor1.value) + '°');
+    $('.current-temp.sensor2').text(Math.round(currentSensor2.value) + '°');
+    $('.current-temp-date span').text(window.moment(currentSensor1.time).format('MMM Do, h:mm:ss a'));
+
+    var trendSensor1 = getTrendWithSensorData(dataSensor1);
+    console.log('Sensor1 Trend: ' + trendSensor1);
+    if (trendSensor1 > 0) {
+      $('.current-temp.sensor1').css({color: colorUp});
+    } else if (trendSensor1 < 0) {
+      $('.current-temp.sensor1').css({color: colorDown});
+    }
+
+    var trendSensor2 = getTrendWithSensorData(dataSensor2);
+    console.log('Sensor2 Trend: ' + trendSensor2);
+    if (trendSensor2 > 0) {
+      $('.current-temp.sensor2').css({color: colorUp});
+    } else if (trendSensor2 < 0) {
+      $('.current-temp.sensor2').css({color: colorDown});
+    }
+  }
+
   // Get data from Pi NodeJS server
   function getSensorData() {
     var sensorData12 = [], sensorData24, sensorData48, time12hrsago, time24hrsago, time48hrsago;
@@ -29,22 +82,27 @@
     time24hrsago = new window.moment().subtract('hours', 24);
     time48hrsago = new window.moment().subtract('hours', 48);
 
-    console.log('get sensor data');
     $.getJSON('./temperature.json', function(response) {
+      //var response = window.testChartData;
       var data = [];
       if (response && response.length > 0) {
         if (response[0] && response[0].length > 0) {
-          data = _.filter(response[0], function(val) { return (val.value > MIN_RANGE && val.value < MAX_RANGE); });
-
-          sensorData12.push(_.filter(data, function(val, index) {
-            return (time12hrsago.isBefore(val.time) && index % 6 === 0);
-          }));
+          sensorData12 = _.filter(response[0], function(val) { return (val.value > MIN_RANGE && val.value < MAX_RANGE); });
         }
         if (response[1] && response[1].length > 0) {
           data = _.filter(response[1], function(val) { return (val.value > MIN_RANGE && val.value < MAX_RANGE); });
-          sensorData12.push(_.filter(data, function(val, index) {
+
+          data = _.map(data, function(item) {
+            var tempData = {};
+            tempData.time = item.time;
+            tempData.value2 = item.value;
+            return tempData;
+          });
+
+          sensorData12 = _.merge(sensorData12, data);
+          sensorData12 = _.filter(sensorData12, function(val, index) {
             return (time12hrsago.isBefore(val.time) && index % 6 === 0);
-          }));
+          });
         }
 
         if (!graph) {
@@ -52,16 +110,17 @@
             element: 'line-example',
             data: sensorData12,
             xkey: 'time',
-            ykeys: ['value', 'value'],
+            ykeys: ['value', 'value2'],
             labels: ['Sensor 1', 'Sensor 2'],
             xLabels: '5min',
-            ymin: 'auto',
-            ymax: 'auto',
+            ymin: 'auto 60.0',
+            ymax: 'auto 72.0',
             hideHover: 'auto',
             postUnits: '°',
             pointSize: 0,
             goals: [60.0, 72.0],
             goalLineColors: ['#00ff00'],
+            yLabelFormat: function (y) { if (y) { return y.toFixed(0) + '°'; } return y; },
             dateFormat: function (x) { return new window.moment(x).format('MMM Do, h:mm:ss a'); }
           });
         } else {
@@ -82,8 +141,7 @@
         sensorData48 = _.pluck(sensorData48, 'value').reverse();
         $('.graph-48hr').sparkline(sensorData48, sparkFormat);
 
-        $('.current-temp').text(Math.round(_.first(response[0]).value) + '°');
-        $('.current-temp-date span').text(window.moment(_.first(response[0]).time).format('MMM Do, h:mm:ss a'));
+        updateCurrentWithResponse(response);
       }
 
     });
